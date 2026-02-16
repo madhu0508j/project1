5d2336540abf4bdf72a646679a22e1dcc3fdba9a
hp <madhuverma0526@gmail.com>
2026-02-16 16:06:57 +0530

    project first

---
+++ Instituteapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def show(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def Course(request):
+    return render(request,'Courses.html')
+
+def admission(request):
+    return render(request,'admission.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def policy(request):
+    return render(request,'policy.html')
---
+++ Instituteproject/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for Instituteproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from Instituteapp.views import show,about,Course,admission,contact,policy
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',show,name='index'),
+    path('about/',about,name='about'),
+    path('courses/',Course,name='courses'),
+    path('admission/',admission,name='admission'),
+    path('contact/',contact,name='contact'),
+    path('Term&Policy/',policy,name='policy')
+]
